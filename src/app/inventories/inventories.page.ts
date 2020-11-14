4dbbb84a00ddc68cfe1a1dcf83807e5cc8ee09d2
Darko Andreev <darko.andreev@paysafe.com>
2020-11-15 01:18:13 +0200

    Select default value fixed

--- src/app/inventories/inventories.page.ts
+++ src/app/inventories/inventories.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CreateEditInventoryFacade } from '../tab2/store/facade/create-edit-inventory.facade';
 import { ICategory } from '../tab2/store/models/category.model';
 import { InventoriesFacade } from './store/facade/inventories.facade';
@@ -10,32 +9,32 @@ import { InventoriesFacade } from './store/facade/inventories.facade';
   styleUrls: ['inventories.page.scss']
 })
 export class InventoriesPage {
-  inventoriesForm: FormGroup;
   selectedCategory: ICategory;
   selectedIsAmortization = false;
+  categoryId: string = '1';
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
               public facadeInventories: InventoriesFacade,
               public facade: CreateEditInventoryFacade) {
-    this.inventoriesForm = this.formBuilder.group ({
-      categoryId: ['', Validators.required],
-      isAmortization: [false]
-    });
   }
 
   ionViewWillEnter(): void {
     this.facade.getCategories();
-    this.facadeInventories.getInventories();
+    this.facadeInventories.getInventories(this.categoryId, this.selectedIsAmortization);
   }
 
-  getInventories(event: any, type: string) {
+  getInventories(event: CustomEvent, type: string) {
     if (type === 'checkbox') {
       this.selectedIsAmortization = event.detail.checked;
     }
-    this.facadeInventories.getInventories(this.inventoriesForm.get('categoryId').value, this.selectedIsAmortization);
+
+    if (type === 'select') {
+      this.categoryId = event.detail.value;
+    }
+    this.facadeInventories.getInventories(this.categoryId, this.selectedIsAmortization);
   }
 
   searchInventory(event: CustomEvent): void {
-    this.facadeInventories.searchInventories(event.detail.value, this.inventoriesForm.get('categoryId').value, this.inventoriesForm.get('isAmortization').value);
+    this.facadeInventories.searchInventories(event.detail.value, this.categoryId, this.selectedIsAmortization);
   }
 }
